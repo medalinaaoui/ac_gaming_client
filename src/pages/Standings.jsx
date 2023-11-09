6c5b4dce8007f3fc56e7e73cb2f44d61b16b8310
Med Ali Naaoui <chayga00@gmail.com>
2023-11-09 05:56:03 +0100

    standing page

--- src/pages/Standings.jsx
+++ src/pages/Standings.jsx
@@ -1,4 +1,75 @@
+import axios from "../../utils/axios";
+import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
+
 const Standings = () => {
-  return <div>Standings</div>;
+  const [standings, setStandings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchStanding = async () => {
+    try {
+      const res = await axios.get(`/standings`);
+      if (res.status === 200) {
+        setIsLoading(false);
+        setStandings(res.data);
+        console.log("Standings ~ res.data:", res.data);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.log("fetchStanding ~ error:", error);
+    }
+  };
+  useEffect(() => {
+    fetchStanding();
+  }, []);
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table ">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>الترتيب</th>
+            <th>الإسم</th>
+            <th>لعب</th>
+            <th>النقط</th>
+          </tr>
+        </thead>
+        <tbody>
+          {isLoading ? (
+            "loading..."
+          ) : standings?.length > 0 ? (
+            standings?.map((user) => (
+              <tr key={user._id}>
+                <th>{user.rank}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={user.teamFlag} alt="main user picture" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">anchof</div>
+                      <div className="text-sm opacity-50">anchof</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {user.gamesPlayed}
+                  <br />
+                  <span className="badge badge-ghost badge-sm">achnof 2</span>
+                </td>
+                <td>{user.points}</td>
+                <td>{user.rank}</td>
+                <td>anchof</td>
+              </tr>
+            ))
+          ) : (
+            <p>You didn&rsquo;t include any vehicle yet</p>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default Standings;

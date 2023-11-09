e0dbc36365d524f81c94017b6a0ecac15abe31a4
Med Ali Naaoui <chayga00@gmail.com>
2023-11-09 20:21:39 +0100

    api url

--- utils/axios.js
+++ utils/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-  baseURL: "https://old-sound-9762.fly.dev/api",
+  baseURL: "https://patient-grass-8418.fly.dev/api",
 
   headers: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
   },
   withCredentials: true,
 });

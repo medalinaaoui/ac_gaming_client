587db0bf7816337d103d2ead70d80ea471f1f4a2
Med Ali Naaoui <chayga00@gmail.com>
2023-11-11 15:36:16 +0100

    color

--- src/pages/Front.jsx
+++ src/pages/Front.jsx
@@ -7,31 +7,31 @@ const Front = () => {
     <div className="min-h-[90vh] flex justify-center items-center">
       <div className="grid sm:grid-cols-4 grid-cols-2 gap-2">
         <Link to="/standings">
-          <article className=" article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+          <article className=" bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
             <LuListOrdered className=" text-7xl" />
             <h2 className=" text-xl font-semibold">الترتيب</h2>
           </article>
         </Link>
         <Link to="/games">
-          <article className=" article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+          <article className=" bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
             <GiSoccerKick className="text-7xl" />
             <h2 className="text-xl font-semibold">المباريات القادمة</h2>
           </article>
         </Link>
         <Link to="/played_games">
-          <article className="  article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+          <article className="  bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
             <GiSoccerBall className="text-6xl" />
             <h2 className="text-xl font-semibold">المباريات الفائته</h2>
           </article>
         </Link>
         <Link to="/join">
-          <article className="relative  article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+          <article className="relative  bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
             <TbSoccerField className="text-7xl" />
             <h2 className="text-xl font-semibold">إنضم لنا</h2>
           </article>
         </Link>
 
-        {/* <article className=" article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+        {/* <article className=" bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
           <img
             src="https://crests.football-data.org/57.png"
             alt="team logo"
@@ -40,7 +40,7 @@ const Front = () => {
           <h1 className="font-bold">Med_ali</h1>
           <h2 className="text-xl font-semibold">أقوى دفاع</h2>
         </article>
-        <article className=" article-con aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
+        <article className=" bg-gray-700 aspect-square rounded-lg p-1 flex flex-col items-center justify-center">
           <img
             src="https://crests.football-data.org/86.png"
             alt="team logo"
